8c29d3ee35c7672afbae1659900fb74f71b3deba
nmai601 <mai@weavy.com>
2022-02-10 14:03:31 -0800

    Revert "Retrying to add weavy-sln files"
    This reverts commit d8e06b7d3b2a2f8a414686596c8db69bec5695fc.

--- src/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Weavy.Global" Language="C#" %>
--- src/Scripts/vendor/jquery/jquery.version.js
+++
@@ -1,1 +0,0 @@
-var jQueryVersion = {"options":{"loose":false,"includePrerelease":false},"loose":false,"raw":"3.6.0","major":3,"minor":6,"patch":0,"prerelease":[],"build":[],"version":"3.6.0"};
